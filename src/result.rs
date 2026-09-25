cfe7b20d8e0b03b5d7d8fc343463e0ccf6013f42
vince <2022020@htlstp.at>
2026-09-25 10:16:10 +0200

    cleaned up unbuffered reader logic

--- src/result.rs
+++ src/result.rs
@@ -1,5 +1,4 @@
 use clap::Parser;
-use crate::args::Args;
 
 #[derive(Parser, Debug)]
 pub struct OutputData {
@@ -8,14 +7,4 @@ pub struct OutputData {
     pub lines: usize,
     pub words: usize,
 }
-
-pub fn assign_to_output_data(args: &Args, bytes: usize, chars: usize, lines: usize, words: usize) -> OutputData {
-    OutputData {
-        bytes: if args.bytes { bytes } else { 0 },
-        chars: if args.chars { chars } else { 0 },
-        lines: if args.lines { lines } else { 0 },
-        words: if args.words { words } else { 0 },
-    }
-}
-
 
--- src/unbuffered_reader.rs
+++ src/unbuffered_reader.rs
@@ -2,59 +2,50 @@ use std::error::Error;
 use std::fs;
 use std::path::Path;
 use crate::args::Args;
-use crate::result::{assign_to_output_data, OutputData};
+use crate::result::OutputData;
 
 pub fn count_single_file_unbuffered(args: &Args) -> Result<OutputData, Box<dyn Error>> {
-    let contents = fs::read_to_string(&args.files)?;
-
-    let output_data = assign_to_output_data(
-        args,
-        contents.len(),
-        contents.chars().count(),
-        contents.lines().count() -1,
-        contents.split_whitespace().count()
-    );
-
-    Ok(output_data)
-}
-
-pub fn count_files_unbuffered(args: &Args) -> Result<OutputData, Box<dyn Error>> {
-    let mut output = OutputData {
+    let mut output_data = OutputData {
         bytes: 0,
         words: 0,
         chars: 0,
         lines: 0,
     };
 
-    args.files.read_dir()?.for_each(|entry| {
-        let path = entry.unwrap().path();
+    read_file(&args.files, &mut output_data)?;
 
-        if path.is_dir() {
-            read_path(&path, &mut output);
-        }
-
-        if path.is_file() {
-            read_file(&path, &mut output);
-        }
-    });
-
-    Ok(output)
+    Ok(output_data)
 }
 
-fn read_path(path: &Path, output_data: &mut OutputData) {
-    for entry in fs::read_dir(path).unwrap() {
-        let path = entry.unwrap().path();
+pub fn count_files_unbuffered(args: &Args) -> Result<OutputData, Box<dyn Error>> {
+    let mut output_data = OutputData {
+        bytes: 0,
+        words: 0,
+        chars: 0,
+        lines: 0,
+    };
+
+    count_dir(&args.files, &mut output_data)?;
+
+    Ok(output_data)
+}
+
+fn count_dir(path: &Path, output_data: &mut OutputData) -> Result<(), Box<dyn Error>> {
+    for entry in fs::read_dir(path)? {
+        let path = entry?.path();
 
         if path.is_dir() {
-            read_path(&path, output_data);
+            count_dir(&path, output_data)?;
         } else if path.is_file() {
-            read_file(&path, output_data);
+            read_file(&path, output_data)?;
         }
     }
+
+    Ok(())
 }
 
-fn read_file(path: &Path, output_data: &mut OutputData) {
-    let content = fs::read_to_string(path).unwrap();
+fn read_file(path: &Path, output_data: &mut OutputData) -> Result<(), Box<dyn Error>>{
+    let content = fs::read_to_string(path)?;
 
     output_data.bytes += content.len();
     output_data.chars += content.chars()
@@ -64,4 +55,6 @@ fn read_file(path: &Path, output_data: &mut OutputData) {
         .count();
     output_data.words += content.split_whitespace()
         .count();
+
+    Ok(())
 }
