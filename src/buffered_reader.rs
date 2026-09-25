250d74be574625c3dab8506dd050b5e7e780fffb
vince <2022020@htlstp.at>
2026-09-25 10:06:56 +0200

    multi file buffered reader implementation

--- src/buffered_reader.rs
+++ src/buffered_reader.rs
@@ -1,18 +1,40 @@
 use std::error::Error;
+use std::fs;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::path::Path;
 use crate::args::Args;
 use crate::result::OutputData;
 
 pub fn count_single_file_buffered(args: &Args) -> Result<OutputData, Box<dyn Error>> {
-    let file = File::open(&args.files)?;
-    let mut reader = BufReader::new(file);
     let mut output_data = OutputData {
         bytes: 0,
         lines: 0,
         words: 0,
         chars: 0
     };
+
+    count_file(&args.files, &mut output_data)?;
+
+    Ok(output_data)
+}
+
+pub fn count_files_buffered(args: &Args) -> Result<OutputData, Box<dyn Error>> {
+    let mut output_data = OutputData {
+        bytes: 0,
+        lines: 0,
+        words: 0,
+        chars: 0
+    };
+
+    count_dir(&args.files, &mut output_data)?;
+
+    Ok(output_data)
+}
+
+pub fn count_file(path: &Path, output_data: &mut OutputData) -> Result<(), Box<dyn Error>> {
+    let file = File::open(path)?;
+    let mut reader = BufReader::new(file);
 
     let mut buffer = Vec::new();
 
@@ -36,5 +58,19 @@ pub fn count_single_file_buffered(args: &Args) -> Result<OutputData, Box<dyn Err
         output_data.words += line.split_whitespace().count();
     }
 
-    Ok(output_data)
+    Ok(())
+}
+
+pub fn count_dir(path: &Path, output_data: &mut OutputData) -> Result<(), Box<dyn Error>> {
+    for entry in fs::read_dir(path)? {
+        let path = entry?.path();
+
+        if path.is_dir() {
+            count_dir(&path, output_data)?;
+        } else if path.is_file() {
+            count_file(&path, output_data)?;
+        }
+    }
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::error::Error;
 use args::Args;
 use clap::{Parser};
 use result::OutputData;
-use crate::buffered_reader::count_single_file_buffered;
+use crate::buffered_reader::{count_files_buffered, count_single_file_buffered};
 use crate::unbuffered_reader::{count_files_unbuffered, count_single_file_unbuffered};
 
 fn main() {
@@ -24,19 +24,20 @@ fn main() {
             std::process::exit(1);
         }
     }
+}
 
 fn run(args: &Args) -> Result<OutputData, Box<dyn Error>> {
-        if args.files.is_file() {
-            return if !args.buffered_read {
-                count_single_file_unbuffered(args)
-            } else {
-                count_single_file_buffered(args)
-            }
+    if args.files.is_file() {
+        if !args.buffered_read {
+            count_single_file_unbuffered(args)
         } else {
-            if !args.buffered_read {
-                return count_files_unbuffered(args);
-            }
+            count_single_file_buffered(args)
         }
-        Err(Box::from("L"))
+    } else {
+        if !args.buffered_read {
+            count_files_unbuffered(args)
+        } else {
+            count_files_buffered(args)
+        }
     }
 }
